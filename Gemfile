598f28730381ab15f2be59c6c18107d12c62b62a
epicodus <student@epicodus.com>
2013-09-27 10:35:41 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails_12factor'
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'bootstrap-sass'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end   
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShortener::Application.config.secret_key_base = '1d3a21fc2d4260138f394404f4bf379a6b0daaa0aa6b12e06761f415a05202e0b480cdfa8368246784686054feecde8135b55c63e40caf082e09add1904a2dd6'
