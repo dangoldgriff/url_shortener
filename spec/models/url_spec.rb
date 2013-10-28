5f53dc548a0f7b2bdcdff27e782f71090285d802
Daniel <daniel@Daniels-MacBook-Pro.local>
2013-10-28 14:37:38 -0700

    update background img and update test

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -4,7 +4,7 @@ describe Url do
   it { should validate_presence_of :long_url }
 
   it 'Should generate a shortened url' do 
-    new_url = Url.create(:long_url => "http:www.epicodus.com")
+    new_url = Url.create(:long_url => "http://www.epicodus.com")
     new_url.short_url.should eq "short" + new_url.id.to_s
   end
 end
